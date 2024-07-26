c066a77fedbd80e801e722a458d8e820e6343491
GhianCo <ghiancorx@gmail.com>
2024-07-25 22:57:51 -0500

    cambios

--- src/Routing/Route.php
+++ src/Routing/Route.php
@@ -11,10 +11,6 @@ use Slim\Factory\ServerRequestCreatorFactory;
 
 class Route
 {
-    private static Request $request;
-
-    private static Router $router;
-
     public static function load(string $routesDirectory) {
         foreach (glob("$routesDirectory/*.php") as $routes) {
             require_once $routes;
@@ -22,64 +18,22 @@ class Route
     }
 
     public static function group(string $uri, $action) {
-        return self::$router->group($uri, $action);
+        return app()->router->group($uri, $action);
     }
 
     public static function get(string $uri, $action) {
-        return self::$router->get($uri, $action);
+        return app()->router->get($uri, $action);
     }
 
     public static function post(string $uri, $action) {
-        return self::$router->post($uri, $action);
+        return app()->router->post($uri, $action);
     }
 
     public static function put(string $uri, $action) {
-        return self::$router->put($uri, $action);
+        return app()->router->put($uri, $action);
     }
 
     public static function delete(string $uri, $action) {
-        return self::$router->delete($uri, $action);
-    }
-
-    public static function source()
-    {
-        self::$router = singleton(Router::class, function () {
-            AppFactory::setContainer(app()->container);
-            $router = AppFactory::create();
-            $router->setBasePath('/' . config("app.path_route"));
-            return $router;
-        });
-
-        self::$request = singleton(Request::class, function () {
-            $serverRequestCreator = ServerRequestCreatorFactory::create();
-            return $serverRequestCreator->createServerRequestFromGlobals();
-        });
-
-        return new static;
-    }
-
-    public function run()
-    {
-        $env = config('app.env');
-
-        // Create Error Handler
-        $errorHandler = new HttpErrorHandler(self::$router->getCallableResolver(), self::$router->getResponseFactory());
-
-        // Create Shutdown Handler
-        register_shutdown_function(new HttpShutdownHandler($this->request, $errorHandler, $env == 'dev'));
-
-        // Add Routing Middleware
-        self::$router->addRoutingMiddleware();
-
-        // Add Body Parsing Middleware
-        self::$router->addBodyParsingMiddleware();
-
-        // Add Error Middleware
-        $errorMiddleware = self::$router->addErrorMiddleware($env == 'dev', false, false);
-        $errorMiddleware->setDefaultErrorHandler($errorHandler);
-
-        // Run App & Emit Response
-        $responseEmitter = new ResponseEmitter();
-        $responseEmitter->emit(self::$router->handle($this->request));
+        return app()->router->delete($uri, $action);
     }
 }
